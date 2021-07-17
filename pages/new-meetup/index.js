4e5f16d0f5979f71ce91e72a79bb21b376e40e50
kminny <darkdevil94@naver.com>
2021-07-18 05:53:54 +0900

    Sending http request

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,21 @@
+import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-  function addMeetupHandler(enteredMeetupDate) {
-    console.log(enteredMeetupDate);
+  const router = useRouter();
+
+  async function addMeetupHandler(enteredMeetupDate) {
+    const response = await fetch('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupDate),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    router.push('/');
   }
 
   return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
