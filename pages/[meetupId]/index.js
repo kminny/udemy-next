d8ba719d142864d473d62b80d19f36e22c3b0861
kminny <darkdevil94@naver.com>
2021-07-17 20:46:34 +0900

    Meetupdetail to new component & using css module on next

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg/1200px-Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The Meetup description"
+    />
+  );
+}
+
+export default MeetupDetails;
