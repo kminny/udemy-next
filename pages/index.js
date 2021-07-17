00736beb10270090b4d2b2a3fde8a454417145a6
kminny <darkdevil94@naver.com>
2021-07-18 06:07:43 +0900

    Getting data from db & keys file added

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,6 @@
+import { MongoClient } from 'mongodb';
 import MeetupList from '../components/meetups/MeetupList';
-
-const DUMMY_MEETUPS = [
-  {
-    id: 'm1',
-    title: 'A First Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg/1200px-Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg',
-    address: 'Some Address 5, 12345, Some City',
-    description: 'This is a first meetup',
-  },
-  {
-    id: 'm2',
-    title: 'A Second Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg/1200px-Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg',
-    address: 'Some Address 5, 12345, Some City',
-    description: 'This is a second meetup',
-  },
-  {
-    id: 'm3',
-    title: 'A Third Meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg/1200px-Vue_de_nuit_de_la_Place_Stanislas_%C3%A0_Nancy.jpg',
-    address: 'Some Address 5, 12345, Some City',
-    description: 'This is a third meetup',
-  },
-];
+import keys from '../keys';
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
@@ -44,9 +19,25 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API
+  const client = await MongoClient.connect(
+    `mongodb+srv://${keys.MONGO_ID}:${keys.MONGO_PW}@cluster0.zzics.mongodb.net/meetups?retryWrites=true&w=majority`
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
   return {
     props: {
-      meetups: DUMMY_MEETUPS,
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
     },
   };
 }
