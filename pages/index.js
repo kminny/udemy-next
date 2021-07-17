20151429acbb0ef15b5e5a10177aeac2bce7526a
kminny <darkdevil94@naver.com>
2021-07-18 07:31:23 +0900

    Head meta added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import { MongoClient } from 'mongodb';
+import Head from 'next/head';
+import { Fragment } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 import keys from '../keys';
 
 function HomePage(props) {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>React Meetups</title>
+        <meta name="description" content="Browse a huge list of highly active React meetups!" />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </Fragment>
+  );
 }
 
 // export async function getServerSideProps(context) {
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,4 +1,6 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { Fragment } from 'react';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
@@ -18,7 +20,15 @@ function NewMeetupPage() {
     router.push('/');
   }
 
-  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+  return (
+    <Fragment>
+      <Head>
+        <title>Add a new Meetup</title>
+        <meta name="description" content="Add your own meetups" />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </Fragment>
+  );
 }
 
 export default NewMeetupPage;
